ee6c17026b450760ce45d4c5765306bfea6a9afb
Denis2025-sv <balabandenis12@gmail.com>
2025-12-11 23:29:59 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import MyButton from "./MyButton";
+import IndicatorTemp from "./IndicatorTemp";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Индикатор Температуры</h1>
+      <IndicatorTemp />
+    </div>
+  );
+}
